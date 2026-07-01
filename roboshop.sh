67c55ae01bcd962caf118b758fb58aa77d112230
joindevopscloud <info@devoos.com>
2026-07-01 19:50:43 +0530

    api doc added

--- roboshop.sh
+++ roboshop.sh
@@ -17,7 +17,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
     
     echo "Instance ID: $INSTANCE_ID"
 
-    if [ $instance == "frontend"]; then
+    if [ $instance == "frontend" ]; then
         IP= $(aws ec2 describe-instances --instance-ids  $INSTANCE_ID \
         --query 'Reservations [*].Instances[*].PublicIpAddress' \
         --output text
