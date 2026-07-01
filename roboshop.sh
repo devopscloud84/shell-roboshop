8eb83815427932b7e3a2362d29a8c9bbfb375092
joindevopscloud <info@devoos.com>
2026-07-01 19:41:32 +0530

    shell-project

---
+++ roboshop.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
+ZON_ID="Z09497833FUO2XY5EW67D"
+DOMAIN_NAME="daws8486.online"
+
+for instance in $@
+do
+
+echo "Launching instance: $Instance"
+INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id ami-0220d79f3f480ecf5 \
+    --instance-type t3.micro \
+    --security-groups "roboshop-common" "roboshop-$Instance" \
+	--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$Instance"}]' \
+	--query 'Instances[0].InstanceId' \
+    --output text)
+    
+    echo "Instance ID: $INSTANCE_ID"
+
+    if [ InstanceId == "frontend"]; then
+        IP= $(aws ec2 describe-instances --instance-ids  $INSTANCE_ID \
+        --query 'Reservations *].Instances[*].PublicIpAddress' \
+        --output text
+        )
+
+
+        R53_RECORD="$DOMAIN_NAME"
+    else
+        IP= $(aws ec2 describe-instances --instance-ids  $INSTANCE_ID \
+        --query 'Reservations *].Instances[*].PrivateIpAddress' \
+        --output text
+        )        
+
+        R53_RECORD="$instance.$DOMAIN_NAME"
+     fi   
+
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZON_ID \
+    --change-batch '
+    {
+        "Changes":
+         [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'R53_RECORD'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                    {
+                    "Value": "$IP"
+                    }
+                ]
+            ]
+        }    
+    '  
+done
