ca8aafb2e7a758b5f10cabc4f874cac3f44ed59d
joindevopscloud <info@devoos.com>
2026-07-07 20:45:09 +0530

    api doc added

--- 02-redis.sh
+++ 02-redis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/logs/roboshop"
+LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
@@ -10,34 +10,34 @@ LOGS_FILE="$LOGS_FOLDER/$0.log"
 
 
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
 echo  -e "$TIMESTAMP [ERROR] $R please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
- VALIDATE(){
-    if [ $1 -ne 0]; then
-        echo -e "$TIMESTAMP [ERROR] $2...$R FAILURE $N" |tee -a $LOGS_FILE
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2...$R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$TIMESTAMP [SUCCESS] $2...$G SUCCESS $N" |tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [SUCCESS] $2...$G SUCCESS $N" | tee -a $LOGS_FILE
     fi
  }
 
-dnf module disable redis -y
-dnf module enable redis:7 -y
-cp mongo.repo  /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Installing redis7"
+dnf module disable redis -y &>> $LOGS_FILE
+dnf module enable redis:7 -y &>> $LOGS_FILE
+dnf install redis -y &>> $LOGS_FILE
+VALIDATE $? "Installing Redis:7"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections"
 
-systemctl enable redis 
-systemctl start redis 
+systemctl enable redis &>> $LOGS_FILE
+systemctl start redis &>> $LOGS_FILE
 VALIDATE $? "Started redis"
