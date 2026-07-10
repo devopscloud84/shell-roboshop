2ccc082c26179bbcd628bc0c2356ca7f182c5a74
joindevopscloud <info@devoos.com>
2026-07-10 19:18:35 +0530

    api doc added

--- robosho-v2.sh
+++ robosho-v2.sh
@@ -3,4 +3,15 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z02249102KA0AGQ9QRMTE" # replace with your zone ID
 DOMAIN_NAME="daws8486.online" # replace with your domain name
+DOMAIN_NAME="daws90s.shop" # replace with your domain name
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
+### Validation ###
+if [ $# -lt 2 ]; then
+    echo -e "$R ERROR:: Atleast 2 arguments required $N"
+    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
+    exit 1
+fi
