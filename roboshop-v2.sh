8707743d53b0bd0a6cd5aa5629b51f7dfcc21264
joindevopscloud <info@devoos.com>
2026-07-19 20:20:11 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -88,7 +88,11 @@ do
             '
             echo "updated R53 record for: $instance"
         else
-            echo "roboshop-$instance already running: $INSTANCE_ID"
+            if [ $INSTANCE_ID == "None" ]; then
+            echo "$instance already destroyed, nothing to do..."
+         else
+            aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+            echo "Terminating Instance: $instance"
         fi
     fi
 done
