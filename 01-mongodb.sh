6c70cc1cb5cc1659390899305cb320b5f4b6acb9
joindevopscloud <info@devoos.com>
2026-07-19 22:07:46 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -1,46 +1,43 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/logs/roboshop"
+LOGS_FOLDER="/var/log/roboshop"
 sudo mkdir -p $LOGS_FOLDER
 sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 
-
-
-
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-if [ $USERID -ne 0]; then
-echo  -e "$TIMESTAMP [ERROR] $R please run this script with root access $N" | tee -a $LOGS_FILE
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
- VALIDATE(){
-    if [ $1 -ne 0]; then
-        echo -e "$TIMESTAMP [ERROR] $2...$R FAILURE $N" |tee -a $LOGS_FILE
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$TIMESTAMP [SUCCESS] $2...$G SUCCESS $N" |tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
- }
+}
 
-cp mongo.repo  /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>> $LOGS_FILE
-VALIDATE $? = "Installing MongoDB"
+VALIDATE $? "Installing MongoDB"
 
 systemctl enable --now mongod
-VALIDATE $? "starting and enabling mongoDB"
+VALIDATE $? "Starting and enabling MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Allowing Remote Connections to MongoDB"
+VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restrat mongod
-VALIDATE $? "Restart MongoDB"
+systemctl restart mongod
+VALIDATE $? "Restarting MongoDB"
